c4386997365b04ed29681eff17beff0b78f09eec
Pushpendra <pushpendrasahu1122@gmail.com>
2023-03-17 01:10:50 +0530

    created home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Show from './Show'
 import '../css/home.css'
 import Sidebar from './Sidebar'
+import Bg from './Bg'
 
 function Home() {
   return (
     <div className='home-container'>
         <Sidebar></Sidebar>
         <Show></Show>
+        {/* <Bg></Bg> */}
+
     </div>
   )
 }
--- src/components/Show.js
+++ src/components/Show.js
@@ -2,26 +2,41 @@ import React from 'react'
 import '../css/show.css'
 
 function Show() {
-  return (
-    <div className='show-container'>
-        <h2>Expense Tracker</h2> 
-        <div className="curr-amount">
-            <h3>Current Balance</h3>
-            <h2> 30 Rs</h2>
+    return (
+        <div className='show-container'>
+            <h2 className='heading'>Expense Tracker</h2>
+            <div className="curr-amount">
+                <h2>CURRENT BALANCE</h2>
+                <h1>Rs 400</h1>
+            </div>
             <div className='both'>
-                <div className='income'>
-                    <h3>Income</h3>
-                    <h2>50 Rs</h2>
+                <div className="income">
+                    <h3>INCOME</h3>
+                    <h1>Rs 600</h1>
                 </div>
-                <div className='expense'>
-                    <h3>Expenses</h3>
-                    <h2>20 Rs</h2>
+                <div className="expense">
+                    <h3>EXPENSES</h3>
+                    <h1>Rs 200</h1>
                 </div>
             </div>
-        </div>
 
-    </div>
-  )
+            <div className="add">
+                <h3 style={{marginBottom:'2vh',borderBottom:'1px solid white'}}>Add New Transaction</h3>
+                <div className='add-text'>
+                    <h5 style={{marginBottom:'-8px'}}>Add Transaction Details</h5>
+                     <input type="text" id='info' placeholder='Add Details'/>
+                </div>
+                <div className='add-amount'>
+                <h5 style={{marginBottom:'-8px'}}>Add Amount <br /> (start amount with '-' sign for expense, e.g -50 for Rs 50 expense)</h5>
+                    <input type="number" id='amount' placeholder='Enter Value'/>  
+                </div>
+                <button id='add-it'>
+                    Add Transaction
+                </button>
+            </div>
+
+        </div>
+    )
 }
 
 export default Show
