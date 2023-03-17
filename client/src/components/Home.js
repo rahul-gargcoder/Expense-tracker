4dc0d48e534cff5a084aeadea5a01e0490cfc03c
Pushpendra <pushpendrasahu1122@gmail.com>
2023-03-17 18:42:34 +0530

    history page created

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,14 +2,17 @@ import React from 'react'
 import Show from './Show'
 import '../css/home.css'
 import Sidebar from './Sidebar'
-import Bg from './Bg'
+import History from './History'
+import Stats from './Stats'
 
 function Home() {
   return (
     <div className='home-container'>
         <Sidebar></Sidebar>
-        <Show></Show>
-        {/* <Bg></Bg> */}
+        {/* <Show></Show> */}
+        {/* <History></History> */}
+        <Stats></Stats>
+        
 
     </div>
   )
