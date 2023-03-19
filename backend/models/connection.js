3545825d8d9b42d7e1179c07a5d0d24276051825
Rahul Garg <96135903+rahul-gargcoder@users.noreply.github.com>
2023-03-19 15:10:41 +0530

    Update connection.js

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 const { userhistoryschema } = require('./userhistory');
 const {userschema}=require('./userschema')
-// mongodb+srv://rahulgarg:expensetracker@cluster0.isrlz9a.mongodb.net/?retryWrites=true&w=majority
+
 // mongodb://localhost:27017/expensetracker
 mongoose.connect('mongodb://localhost:27017/expensetracker')
 .then(function(db){
